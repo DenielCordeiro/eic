c24f435d7445b08535692a3fceac7b18d98861a1
TI | Grupo Simeira <ti01@gruposimeira.com.br>
2026-09-18 15:23:21 -0300

    feat[Inventory]: Service to CRUD data from sheet google

---
+++ src/app/services/inventory.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Machine } from '../models/machine';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InventoryService {
+  // Substitua com a sua URL do Google Apps Script (a que termina em /exec)
+  private apiUrl = 'SUA_URL_DO_GOOGLE_APPS_SCRIPT_AQUI';
+
+  constructor(private http: HttpClient) {}
+
+  // Busca a lista de máquinas cadastradas
+  getMachines(): Observable<Machine[]> {
+    return this.http.get<Machine[]>(this.apiUrl);
+  }
+
+  // Envia uma nova máquina para ser gravada na planilha
+  addMachine(maquina: Machine): Observable<any> {
+    // Usamos text/plain para contornar restrições de CORS do Apps Script
+    const headers = new HttpHeaders({ 'Content-Type': 'text/plain' });
+    return this.http.post(this.apiUrl, JSON.stringify(maquina), { headers });
+  }
+}
