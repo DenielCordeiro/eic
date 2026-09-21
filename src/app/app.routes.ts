6aa3fdd0084675424e13c27ec18a009e5b8fac37
TI | Grupo Simeira <ti01@gruposimeira.com.br>
2026-09-21 12:37:07 -0300

    <feat>[

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'machines',
+    pathMatch: 'full'
+  },
+  {
+    path: 'machines',
+    loadComponent: () =>
+      import('./components/machines/machines').then(m => m.Machines)
+  },
+  {
+    path: '**',
+    redirectTo: 'machines'
+  }
+];
