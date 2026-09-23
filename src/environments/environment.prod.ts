7e06ad8470fe170a05562dc2fc08068a74bec6e3
TI | Grupo Simeira <ti01@gruposimeira.com.br>
2026-09-23 16:25:33 -0300

    refactor[Environment]: varibles for next task

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,8 @@ export const environment = {
     production: true,
         // API URL do Google Sheets para o inventário de máquinas
         machinesInventoryUrl: 'https://script.google.com/macros/s/AKfycbwGehk7nhpsNDeKLYmUzaT2ar5oIXiY1EaAG7hB5Z0dBwkSZCYkKGIff1tsRGs46ren/exec',
-    };
+        // API URL do Google Sheets para o inventário de periféricos
+        peripheralsInventoryUrl: '',
+        // API URL do Google Sheets para o inventário de dispositivos móveis
+        mobileInventoryUrl: ''
+};
